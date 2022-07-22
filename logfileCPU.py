8f597da86a9e3db99abb44d6a530a56d8042e799
Mohammad Imaran <90084061+imaranresearch@users.noreply.github.com>
2022-07-22 11:50:40 +0100

    Update logfileCPU.py

--- logfileCPU.py
+++ logfileCPU.py
@@ -12,11 +12,14 @@ def cpu_timebreakdown(str):
     for line in contents:
         if (line.startswith(cpu_time_strings)==True):
             data1 = (line.split('|'))
-            # print(data1[0],data1[len(data1)-1])
+          
             algorithm.append(data1[0].strip())
             relative_time_percentage.append(float(data1[len(data1)-1]))
+            
         if (line.startswith(cpu_use_string)):
             wall_time=wall_time+(float(line.split()[3]))
+            
+            
     cpu_use_percentage = pd.DataFrame([np.array(relative_time_percentage)],columns=np.array(algorithm))
 
     return cpu_use_percentage,wall_time
